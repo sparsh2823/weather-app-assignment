e6e92765c8fd37950ca9db1ecf02e45699a3e362
Sparsh Singh Rathore <sparsh2823@gmail.com>
2022-06-22 00:51:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./Components/Header/Header";
+import Modal from "./Components/Modal/Modal";
+import CityList from "./Components/CityList/CityList"
+import {useState , createContext} from "react";
+import axios from "axios";
+import WeatherReport from './Page/WeatherReport';
+// import { Routes, Route } from "react-router-dom";
+
+
+
+export const GlobalInfo = createContext();
 
 function App() {
+
+  const [openModal,setOpenModal] = useState(false);
+  const [inputValue,setInputValue] = useState("");
+  const [listOfCities,setListOfCities] = useState([]);
+  const [weatherData, setWeatherData] = useState({
+        description: "",
+        temp: "",
+        temp_max: "",
+        temp_min: "",
+        humidity: "",
+        sunrise: "",
+        sunset: "",
+        country: "",
+    })
+
+   const fetchWeatherReport=(city)=>{
+
+         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a035415c04a07118338b51b17a27ff9b`)
+        .then((response)=>{
+           setWeatherData({
+                    description: response.data.weather[0].description,
+                    temp: response.data.main.temp - 273,
+                    temp_max: response.data.main.temp_max,
+                    temp_min: response.data.main.temp_min,
+                    humidity: response.data.main.humidity,
+                    sunrise: response.data.sys.sunrise,
+                    sunset: response.data.sys.sunset,
+                    country: response.data.sys.country,
+                    
+                })
+        })
+  }
+
+
+
+
   return (
+    <>
+    
+    <GlobalInfo.Provider
+     value ={{inputValue,setInputValue,listOfCities,setListOfCities,weatherData,setWeatherData,fetchWeatherReport}}>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    
+    <Header onOpenModal={setOpenModal}/>
+    {openModal && <Modal onCloseModal={setOpenModal}/>}
+    <CityList/>
+    <WeatherReport weatherData={weatherData}/>
     </div>
+    
+    </GlobalInfo.Provider>
+    
+    
+    
+    </>
   );
 }
 
